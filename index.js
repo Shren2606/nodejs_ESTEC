8666a6d51e40a87c8c9f56895649ed651f927233
Lê Hoàng Thiện <lethien2606@example.com>
2023-03-11 17:15:11 +0700

    express handlebars

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
   return res.send('Hello World')
 })
 
-app.listen(port,() => console.log(`App listerning http://localhost:${port}`))
+app.listen(port,() => console.log(`App listening http://localhost:${port}`))
